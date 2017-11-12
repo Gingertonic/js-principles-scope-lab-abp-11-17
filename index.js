1a25dc8c965449179f6519cb21ef2f3b94b917ac
Beth Schofield <bethmschofield@gmail.com>
2017-11-12 11:46:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var customerName = "bob";
 
-function upperCaseCustomerName(customerName) {
-  var customerName = customerName.toUpperCase()
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
 }
